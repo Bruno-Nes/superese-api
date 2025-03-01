a10c808a0cb97fe9e8b43a9a5bd0a42eb933ce5c
Bruno Nestor Celino <bruno.nestor@aerohouse.com.br>
2025-03-01 12:28:50 -0300

    feat: generate token when register a user

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -8,6 +8,7 @@ import {
 } from '@nestjs/common';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
 import { AuthService } from '../auth.service';
+import { Public } from 'src/lib/decorators/public-route.decorators';
 
 @Controller('auth')
 export class AuthController {
@@ -15,6 +16,7 @@ export class AuthController {
 
   @Post()
   @HttpCode(HttpStatus.OK)
+  @Public()
   @UseGuards(LocalAuthGuard)
   async login(@Request() req: any) {
     return this.authService.login(req.user);
