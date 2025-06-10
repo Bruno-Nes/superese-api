e490a341cc43370cb6968f91179dcc62ea7d5ae1
Bruno Nestor Celino <bruno.nestor@aerohouse.com.br>
2025-06-10 11:55:11 -0300

    alteracoes

--- src/modules/planner/planner.module.ts
+++ src/modules/planner/planner.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Goal } from './entities/goal.entity';
-import { Achievement } from './entities/achievement.entity';
-import { Medal } from './entities/medal.entity';
 import { Plan } from './entities/plan.entity';
 import { Observation } from './entities/observation.entity';
 import { PlannerController } from './controllers/planner.controller';
@@ -10,16 +8,7 @@ import { PlannerService } from './services/planner.service';
 import { Profile } from '@modules/user/entities/profile.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      Goal,
-      Achievement,
-      Medal,
-      Plan,
-      Observation,
-      Profile,
-    ]),
-  ],
+  imports: [TypeOrmModule.forFeature([Goal, Plan, Observation, Profile])],
   controllers: [PlannerController],
   providers: [PlannerService],
   exports: [PlannerService],
