09bcd21aba54dfe8fae5d6313fa55684cfcb110f
Bruno Nestor Celino <bruno.nestor@aerohouse.com.br>
2025-06-15 09:58:40 -0300

    change cors origin

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
+      'https://superese-fire.web.app',
       'http://localhost:8100',
       'http://localhost:8101',
       'http://localhost:8102',
